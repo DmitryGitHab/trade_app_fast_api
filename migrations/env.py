70c6481b332fc6206d4736456228547cff904c73
Dmitry <babanov91@gmail.com>
2023-11-19 21:37:12 +0500

    ws add cht

--- migrations/env.py
+++ migrations/env.py
@@ -15,6 +15,8 @@ from src.config import DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASS
 # access to the values within the .ini file in use.
 from src.auth.models import metadata as metadata_auth
 from src.operations.models import metadata as metadata_operations
+from src.chat.models import *
+from src.database import metadata, Base
 
 config = context.config
 
@@ -34,7 +36,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = [metadata_auth, metadata_operations]
+target_metadata = [metadata_auth, metadata_operations, Base.metadata]
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
